99fdcd27117e900e7a7e932afd7ef812dd83bac1
Jorg Jenni <jorg.jenni@jennius.co.uk>
2025-12-15 13:07:21 +0000

    add General Linear Systems / Exercise 6

--- src/guide_to_linear_algebra/general_linear_systems_functions.py
+++ src/guide_to_linear_algebra/general_linear_systems_functions.py
@@ -2,14 +2,30 @@ import numpy as np
 from . import laguide as lag
 
 
-def NrFreeVariables(E):
-    R = lag.FullRowReduction(E)
-    nr_bound_vars = 0
-    nr_columns = R.shape[1]
-    for i, row in enumerate(R):
-        if np.all(row[:-1] == 0) and row[-1] != 0:
-            return 0  # inconsistent system
-        if row[i] == 1 and np.sum(row[:i] == 0):
-            nr_bound_vars += 1
+def find_pivots(R):
+    """
+     Find the pivot columns in the row echelon form R of a matrix.
+    """
+    pivots = []
 
-    return nr_columns - 1 - nr_bound_vars
+    for row in R[:, :-1]:  # Exclude the last column (augmented part)
+        if np.all(row == 0):
+            continue  # Skip zero rows
+
+        idx_none_zeros = np.nonzero(row != 0)[0]
+        idx_one = np.nonzero(row == 1)[0][0]
+
+        if np.all(idx_none_zeros >= idx_one):
+            # a 1 only preceded by zeros
+            pivots.append(idx_one)
+
+    return np.array(pivots)
+
+
+def nr_free_variables(E):
+    # Find the number of columns in R that do not contain a pivot
+    pass
+    R = lag.FullRowReduction(E)
+    P = find_pivots(R)
+
+    return R.shape[1] - len(P) - 1
---
+++ tests/test_general_linear_systems_functions.py
@@ -0,0 +1,62 @@
+import pytest
+import numpy as np
+from guide_to_linear_algebra.general_linear_systems_functions import nr_free_variables
+from guide_to_linear_algebra.general_linear_systems_functions import find_pivots
+
+
+def test_find_pivots_example_1():
+    E = np.array([[1, 0, 0, 5],
+                  [0, 1, 0, -3],
+                  [0, 0, 1, 2]])
+
+    assert np.array_equal(find_pivots(E), [0, 1, 2])
+
+
+def test_find_pivots_example_2():
+    E = np.array([[1, 0, 0, 5],
+                  [0, 1, 0, -3],
+                  [0, 0, 0, 0]])
+
+    assert np.array_equal(find_pivots(E), [0, 1])
+
+
+def test_find_pivots_example_3():
+    E = np.array([[1, 2, 0, 4],
+                  [0, 0, 1, -1],
+                  [0, 0, 0, 0]])
+    assert np.array_equal(find_pivots(E), [0, 2])
+
+
+def test_find_pivots_example_4():
+    E = np.array([[1, 3, -2, 7],
+                  [0, 0, 0, 0],
+                  [0, 0, 0, 0]])
+    assert np.array_equal(find_pivots(E), [0])
+
+
+def test_find_pivots_example_5():
+    E = np.array([[0, 1, 2, 3],
+                  [0, 0, 0, 0],
+                  [0, 0, 0, 0]])
+    assert np.array_equal(find_pivots(E), [1])
+
+
+def test_nr_free_variables_unique_solution():
+    E = np.array([[1, 0, 0, 5],
+                  [0, 1, 0, -3],
+                  [0, 0, 1, 2]])
+    assert nr_free_variables(E) == 0
+
+
+def test_nr_free_variables_one_free_variable():
+    E = np.array([[-2, 2, -2, 2, 0],
+                  [1, -2, -2, 0, -1],
+                  [1, 0, 2, -2, 1]])
+    assert nr_free_variables(E) == 1
+
+
+def test_nr_free_variables_two_free_variables():
+    E = np.array([[-2, 4, 2, -8, 4, -8],
+                  [3, -6, -2, 11, -7, 13],
+                  [1, - 2, -5, 8, 1, -3]])
+    assert nr_free_variables(E) == 2
